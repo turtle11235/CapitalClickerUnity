fc7a5f479b350ce930dd7f235728ea521d445c99
Emrys Shevek <mpoggemann96@gmail.com>
2022-06-27 14:42:09 -0700

    initial commit

---
+++ Assets/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public double money;
+    public double clickVal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        money = 0;
+        clickVal = .01;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (money >= clickVal * 10)
+        {
+            clickVal *= 10;
+        }
+    }
+}
---
+++ Assets/MoneyButtonBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyButtonBehaviour : MonoBehaviour
+{
+    public Text moneyText;
+    public Game game;
+    
+    public void OnButtonPress()
+    {
+        game.money += game.clickVal;
+        moneyText.text = "$" + game.money;
+    }
+}
